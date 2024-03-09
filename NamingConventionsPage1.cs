8762a4a3f1d1c2c68d026904827c4cf72db66905
Manoj <askbotta@gmail.com>
2024-03-09 14:29:16 +0530

    Add project files.

---
+++ NamingConventionsPage1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConventions
+{
+    public class NamingConventionsPage1
+    {
+        public void NamingConventions()
+        {
+            /* Variable names should use camel case
+            Variable names should be descriptive and meaningful in your application
+            You likely write your code once, but need to read the code multiple times.
+            Use whitespace judiciously to improve the readability of your code.
+                Use line feeds to create empty lines to separate phrases of code. A phrase includes lines of code that are similar, or work together.
+                Use line feeds to separate code block symbols so that they are on their own line of code.
+                Use the tab key to line up a code block with the keyword they're associated with.
+                Indent code inside of a code block to show ownership.
+            */
+
+
+
+            
+
+        }
+    }
+}
